9c2936effe29aa4104832139a30cdd1abf78cf27
Conrad-C <jvococonrad@gmail.com>
2021-03-08 20:32:57 -0300

    Select card interface

--- pages/games/[gameName]/playing.js
+++ pages/games/[gameName]/playing.js
@@ -4,34 +4,24 @@ import React, { Component, useEffect, useState } from 'react';
 import { socket } from '../../index'
 
 
-
+socket.emit('getGameData',123);
 const Game = ({}) => {
 
     //Variables
     const router = useRouter();
     const [users, setUsers] = useState([]);
     const [game, setGame] = useState({});
+    const [selecting, setSelecting] = useState(false);
+    const [cards, setCards] = useState([]);
     const [player, setPlayer] = useState({});
     const {auth, name, gameName} = useSelector((state) => state.user);
 
     //useEffects
     useEffect(() => {
-        socket.emit('getGameData',123);
-    },[game]);
-
-    useEffect(() => {
-        socket.on('userDisconnected',(data)=>{
-            console.log(data);
-        })
-        return () => {
-            socket.off;
-          }
-    }, [game]);
-
-    useEffect(() => {
-        socket.on('gameDataRes'+123, data =>{
+        socket.on('gameDataRes'+router.query.gameName, data =>{
             setUsers(data.users);
             setGame(data);
+            setCards(data.cards);
             users.forEach(element => {
                 if(element.name == name){
                     setPlayer(element);
@@ -43,20 +33,32 @@ const Game = ({}) => {
           }
     }, [users], [game]);
 
+    useEffect(() => {
+        socket.on('newTurn'+router.query.gameName, data =>{
+            setGame(data);
+        })
+        return () => {
+            socket.off;
+          }
+    }, [game]);
 
     //Functions
-    const bigode = (tag) =>{
-        if(tag == 'Hitler' && player.tag != 'Liberal'){
-            return <div className="bigode"></div>
-        }else{
-            return null;
-        }
-        
+    function newTurn(){
+        setSelecting(true);
     }
-    
 
     //Page render
     return <div className="game-body">
+        <div style={selecting ? {display:'flex'} : {display: 'none'}} className="select-card-president"> 
+            {cards.slice(0, 3).map((value, index) => {
+                if(value == 0){
+                    return <div key={index} style={{width:'30%',marginRight:0}} className="fascist-law"></div>
+                }
+                else if(value == 1){
+                    return <div key={index} style={{width:'30%',marginRight:0}} className="liberal-law"></div>
+                }
+            })}
+        </div>
         <div className="other-players-top">
             {users.map((value, index) => {
                 if(value.name != name) {
@@ -65,10 +67,14 @@ const Game = ({}) => {
                     style={{backgroundColor:"#B39C4D"}} 
                     className={"userBox " + (player.tag == 'Liberal' ? "" : value.tag)}
                     >{value.name}
-                    {bigode(value.tag)}</div>}})}
+                    {value.tag == 'Hitler' && player.tag != 'Liberal' ? <div className="bigode"></div> : null}</div>}})}
         </div>
         <div className="cards-mid">
-            <div title="Wait your turn to buy!" className="buy-cards"></div>
+            <div 
+                onClick={game.actualTurn && game.actualTurn.name == name ? newTurn : null}
+                title="Wait your turn to buy!" 
+                className={"buy-cards " + (game.actualTurn && game.actualTurn.name == name ? "card-active" : "")}
+                ></div>
             <div className="discard-cards"></div>
             <div className="laws-aligner">
                 <div className="liberal-laws">
@@ -91,7 +97,7 @@ const Game = ({}) => {
                 style={{backgroundColor:"#B39C4D"}} 
                 className={`userBox ${value.tag}`}
                 >{value.name}
-                {bigode(value.tag)}
+                {value.tag == 'Hitler' ? <div className="bigode"></div> : null}
                 </div>
             }
         })}
